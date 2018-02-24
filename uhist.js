623e1ea4e7f6cdd8e1a7e6e5bff5e3c91d2956ff
aescherling <adam.scherling@gmail.com>
2018-02-24 10:50:39 -0800

    uhist is essentially complete, just need to clean up the procedure for dealing with factor variables

--- uhist.js
+++ uhist.js
@@ -3,9 +3,13 @@
 var myData;
 var myVar;
 
+// globally accessible bin number variable
+var currentBins = 10;
+var currentVar;
+
 // create an svg - everything will be drawn on this
 var svg = d3.select('#viz').append('svg')
-	.attr('height', '400px')
+	.attr('height', '550px')
 	.attr('width', '700px');
 
 // add text elements for warnings about the data load
@@ -15,11 +19,11 @@ var missingnessWarning = svg.append('text').attr('y', 50);
 // create a histogram group
 var histogram = svg.append('g')
 	.attr('id','histogram')
-	.attr('transform','translate(100,100)');
+	.attr('transform','translate(200,100)');
 
 // histogram dimensions
-var histHeight = 200;
-var histWidth = 200;
+var histHeight = 300;
+var histWidth = 300;
 
 // draw axes of histogram
 // default to (0,1) scale
@@ -34,29 +38,12 @@ histogram.append('g')
   .attr('transform', 'translate(0,' + histHeight + ')')
   .call(d3.axisBottom(x_scale).ticks(5));
 
-// create a scatterplot group
-var scatterplot = svg.append('g')
-	.attr('id', 'scatterplot')
-	.attr('transform', 'translate(400, 100)');
-
-// draw axes of scatterplot
-scatterplot.append('g')
-  .attr('class', 'axis yAxis')
-  .call(d3.axisLeft(y_scale).ticks(5));
-
-scatterplot.append('g')
-  .attr('class', 'axis xAxis')
-  .attr('transform', 'translate(0,' + histHeight + ')')
-  .call(d3.axisBottom(x_scale).ticks(5));
-
-// label the y axis of the scatterplot
-scatterplot.append('text')
-	.attr('id', 'yAxisLabel')
-	.attr("x", -40)
-    .attr("y", 100)
-    .attr('transform','rotate(-90 -40,100)')
+// add a label on the x axis
+histLabel = histogram.append('text')
+	.attr("x", 150)
+    .attr("y", 340)
     .attr("text-anchor", "middle")
-    .text("");
+    .attr("text", "");;
 
 
 // function for calculating the counts necessary for producing a histogram
@@ -81,13 +68,13 @@ function calcHist(x, low, high, bins) {
 
 // function for updating the histogram with input variable (array) x
 // also updates the y variable of the scatterplot
-function updateHist(x) {
+function updateHist(x, bins, dur) {
 
 	var xMin = d3.min(x);
 	var xMax = d3.max(x);
 
 	// calculate the counts of the chosen variable using calcHist
-	var bins = 10;
+	// var bins = 10;
 	var counts = calcHist(x, Math.round(xMin)-1, Math.round(xMax)+1, bins);
 	myVar = counts;
 	var stepSize = (Math.ceil(xMax) - Math.floor(xMin)) / bins;
@@ -103,50 +90,21 @@ function updateHist(x) {
 	// update bars
     d3.selectAll('.bar')
       .transition()
-      .duration(100)
+      .duration(dur)
       .attr('height', function (d,i) {return histHeight - histScaleY(counts[i])})
       .attr('y', function (d,i) {return histScaleY(counts[i])});
 
-
-	// relabel y axis of scatterplot and update points
-	scatterScaleY = d3.scaleLinear().domain([Math.floor(xMin), Math.ceil(xMax)]).range([histHeight, 0]);
-	d3.select('#scatterplot').select('.yAxis').call(d3.axisLeft(scatterScaleY).ticks(5));
-	d3.select('#scatterplot').selectAll('.point')
-		.transition()
-		.duration(100)
-		.attr('cy', function(d,i) {return scatterScaleY(x[i])})
-		.attr('stroke', 'black');
-
 } // end of updateHist()
 
-
-// function for updating the x variable of the scatterplot with input variable (array) x
-function updateScatter(x) {
-
-	var xMin = d3.min(x);
-	var xMax = d3.max(x);
-
-	// rescale/relabel x axis
-	scatterScaleX = d3.scaleLinear().domain([Math.floor(xMin), Math.ceil(xMax)]).range([0, histWidth]);
-	d3.select('#scatterplot').select('.xAxis').call(d3.axisBottom(scatterScaleX).ticks(5));
-
-	// update points
-	d3.selectAll('.point')
-		.transition()
-		.duration(100)
-		.attr('cx', function(d,i) {return scatterScaleX(x[i])})
-		.attr('fill', 'black');
-
-} // end of updateHist()
 
 
 // function for creating a variable selector
-function makeSelector(id, data, variables, mousemove, transform) {
+function makeVariableSelector(id, data, variables, transform) {
 	// if a selector with this id already exists, remove it
 	d3.selectAll('#' + id).remove();
 
 	// set dimensions
-    var width = 200;
+    var width = histWidth;
     var height = 20;
     var textwidth = 55;
     var margin = 5;
@@ -170,6 +128,11 @@ function makeSelector(id, data, variables, mousemove, transform) {
 		.attr('id', id)
 		.attr('transform', transform);
 
+	selector.append('text')
+		.attr('x', -80)
+		.attr('y', 12)
+		.text('variable: ');
+
 	// add the line to the selector
 	var path = selector.append('path')
       .datum(variables)
@@ -179,98 +142,127 @@ function makeSelector(id, data, variables, mousemove, transform) {
     // dot shows the current variable selection
     var dot = selector.append("circle")
     	.attr('class', 'dot')
-        .attr('cx', function() {
-        	if (mousemove=="histogram") {
-        		out = x(0);
-        	} else if (mousemove=="scatterplot") {
-        		out = x(1);
-        	}
-        	return(out);
-        })
+        .attr('cx', x(0))
         .attr('cy', y(0))
         .attr("r", 5);
 
-    // variable name
-    var varName = selector.append("text")
-    	.attr("x", 100)
-    	.attr("y", -10)
-    	.attr("text-anchor", "middle")
-    	.attr("text", "");
-
-	// function for updating the histogram and y variable of scatterplot using the selector
+	// function for updating the histogram using the selector
 	function moveHist() {
 		var i = Math.min(Math.round(x.invert(d3.mouse(this)[0])), variables.length-1);
 		dot.attr('cx', x(i)).attr('cy', y(i));
-		varName.text(variables[i]);
-		d3.select('#yAxisLabel').text(variables[i]);
+		histLabel.text(variables[i]);
 	
 		// update the histogram to view the selected variable
-		var newVar = data.map(function(d) {return +d[variables[i]]});
-		updateHist(newVar);
+		currentVar = data.map(function(d) {return +d[variables[i]]});
+		updateHist(currentVar, currentBins, 100);
 	}
 
-	// function for updating the x variable of the scatterplot using the selector
-	function moveScatter() {
-		var i = Math.min(Math.round(x.invert(d3.mouse(this)[0])), variables.length-1);
-		dot.attr('cx', x(i)).attr('cy', y(i));
-		varName.text(variables[i]);
-	
-		// update the histogram to view the selected variable
-		var newVar = data.map(function(d) {return +d[variables[i]]});
-		updateScatter(newVar);
-	}
 
     var overlay = selector.append("rect")
         .attr("class", "overlay")
         .attr("width", width)
         .attr("height", height)
-        .attr("style", "fill: none; pointer-events: all;");
-
-    if (mousemove=="histogram") {
-        overlay.on("mousemove", moveHist);
-    } else if (mousemove=="scatterplot") {
-    	overlay.on("mousemove", moveScatter);
-    }
+        .attr("style", "fill: none; pointer-events: all;")
+        .on("mousemove", moveHist);;
 
     return(selector);
-} // end of makeSelector();
+} // end of makeVariableSelector();
+
+
+// function for creating a variable selector
+function makeBinSelector(id, transform) {
+	// if a selector with this id already exists, remove it
+	d3.selectAll('#' + id).remove();
+
+	// set dimensions
+    var width = histWidth;
+    var height = 20;
+    var textwidth = 55;
+    var margin = 5;
+
+    // bin size options: 5 to 35, by 5
+    binOptions = Array.apply(null, Array(7)).map(function (_, i) {return 5*i + 5;});
+
+	// create the scales
+    var x = d3.scaleLinear()
+    	.domain([0, binOptions.length - 1])
+    	.range([0, width - 2*margin]);
+
+    var y = d3.scaleLinear()
+    	.domain([0, 1])
+    	.range([height/2, height/2]);
+
+    // a line based on the scales
+    var line = d3.line()
+        .x(function(d, i) { return x(i); })
+        .y(function(d, i) { return y(i); });
+
+    // create a selector group
+	var selector = svg.append('g')
+		.attr('id', id)
+		.attr('transform', transform);
+
+	selector.append('text')
+		.attr('x', -80)
+		.attr('y', 12)
+		.text('# bins: ');
+
+	// add the line to the selector
+	var path = selector.append('path')
+      .datum(binOptions)
+      .attr('d', line)
+      .attr('style', 'fill: none; stroke: #000; stroke-width: 0.5px;');
+
+    // dot shows the current variable selection
+    var dot = selector.append("circle")
+    	.attr('class', 'dot')
+        .attr('cx', x(1))
+        .attr('cy', y(0))
+        .attr("r", 5);
+
+	// function for updating the histogram using the selector
+	function changeBins() {
+		var i = Math.min(Math.round(x.invert(d3.mouse(this)[0])), binOptions.length-1);
+		dot.attr('cx', x(i)).attr('cy', y(i));
+	
+		// check to make sure the # of bins has changed
+		if (currentBins != binOptions[i]) {
+			// update the number of bins
+			currentBins = binOptions[i];
+
+			// remove the existing bars
+			d3.select('#histogram').selectAll('.bar').remove();
+
+			// add bars with height 0 to the histogram
+			var barWidth = histWidth / currentBins;
+			d3.select('#histogram').selectAll('.bar')
+			    .data(Array(currentBins))
+			    .enter()
+			    .append('rect')
+			    .attr('class', 'bar')
+			    .attr('width', barWidth)
+			    .attr('height', 0)
+			    .attr('x', function (d, i) {return i * barWidth})
+			    .attr('y', histHeight)
+			    .attr('fill', 'steelblue')
+			    .attr('stroke', d3.rgb('steelblue').darker());
+
+			// update the histogram values
+			updateHist(currentVar, currentBins, 0);
+		}
+	}
 
 
 
-// when the data have loaded, explore!
-// queue()
-//   .defer(d3.csv, 'myData.csv')
-//   .await(explore);
+    var overlay = selector.append("rect")
+        .attr("class", "overlay")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("style", "fill: none; pointer-events: all;")
+        .on("mousemove", changeBins);;
 
-// alternatively, upload a file
-// http://bl.ocks.org/syntagmatic/raw/3299303/
-// function upload_button(el, callback) {
-//   var uploader = document.getElementById(el);  
-//   var reader = new FileReader();
-
-//   reader.onload = function(e) {
-//     var contents = e.target.result;
-//     callback(contents);
-//   };
-
-//   uploader.addEventListener("change", handleFiles, false);  
-
-//   function handleFiles() {
-//     var file = this.files[0];
-//     reader.readAsText(file);
-//   };
-// };
-// upload_button('files', load_data);
-
-// // load dataset and explore!
-// function load_data(csv) {
-//   // clear any warning messages
-//   categoricalWarning.text('');
-//   missingnessWarning.text('');
-//   // load the data and explore!
-//   var data = d3.csvParse(csv);
-//   explore(data);
-// }
+    return(selector);
+} // end of makeBinSelector();
 
 
 
@@ -281,11 +273,11 @@ function explore(data) {
 	// get a list of the variable names
 	var varNames = Object.keys(data[0]);
 
-	// convert all categorical variables to numeric
+	// convert all factor variables to numeric
 	// if a column contains no numbers, find all unique values (other than "NA", "", or ".") and assign a number
 	var n = data.length;
 	var p = varNames.length;
-	var categoricalVars = [];
+	var factorVars = [];
 	// loop over each column
 	for (j=0; j<p; j++) {
 		// classify each observation in the column as missing, non-numeric, or numeric
@@ -307,9 +299,9 @@ function explore(data) {
 			}
 			return(out);
 		});
-		// if nNaN/n > 0.5, classify as a categorical variable. assign a number to each unique value
+		// if nNaN/n > 0.5, classify as a factor variable. assign a number to each unique value
 		if (nNaN/n > 0.5) {
-			categoricalVars.push(varNames[j]);
+			factorVars.push(varNames[j]);
 			// create an array for the jth variable
 			var tmpVar = data.map(function(d) {return(d[varNames[j]]);});
 			// find the unique values
@@ -325,10 +317,10 @@ function explore(data) {
 	}
 
 	// display a message if any variables were identified as categorical
-	if (categoricalVars.length > 0 & categoricalVars.length < 4) {
-		categoricalWarning.text('The following variables have been identified as categorical variables: ' + categoricalVars);	
-	} else if (categoricalVars.length > 3) {
-		categoricalWarning.text('Warning: ' + categoricalVars.length + ' columns were identified as categorical variables.');
+	if (factorVars.length > 0 & factorVars.length < 4) {
+		categoricalWarning.text('The following variables have been identified as factor variables: ' + factorVars);	
+	} else if (factorVars.length > 3) {
+		categoricalWarning.text('Warning: ' + factorVars.length + ' columns were identified as categorical variables.');
 	}
 	
 
@@ -352,33 +344,27 @@ function explore(data) {
 	data = newData;
 
 	// display a message if any rows were removed
-	if (categoricalVars.length > 0 & categoricalVars.length < 4) {
-		missingnessWarning.text('The following observations have been removed due to missing data: ' + uniqueRows);
-	} else if (categoricalVars.length > 3) {
-		missingnessWarning.text('Warning: ' + uniqueRows.length + ' observations were removed due to missing data.');
+	if (missingRows.length > 0 & missingRows.length < 4) {
+		missingnessWarning.text('The following observations have been removed due to missing data: ' + missingRows);
+	} else if (missingRows.length > 3) {
+		missingnessWarning.text('Warning: ' + missingRows.length + ' observations were removed due to missing data.');
 	}
 	
 
 
-	// remove all histogram bars and scatterplot points
+	// remove all histogram bars
 	d3.select('#histogram').selectAll('.bar').remove();
-	d3.select('#scatterplot').selectAll('.point').remove();
 
+    // create a toolbar for switching the histogram variable
+    var selector1 = makeVariableSelector('selector1', data, varNames, 'translate(207,450)');
 
-    // create a toolbar for switching the histogram variable (and y variable of scatterplot)
-    var selector1 = makeSelector('selector1', data, varNames, 'histogram', 'translate(105,350)');
-
-    // create a toolbar for switching the x variable of scatterplot
-    var selector2 = makeSelector('selector2', data, varNames, 'scatterplot', 'translate(405,350)');
-
-	// create crossfilter
-	// var cf = crossfilter(data);
+    // create a toolbar for changing number of bins
+    var selector2 = makeBinSelector('selector2', 'translate(207,490)');
 
 	// add bars with height 0 to the histogram
-	var bins = 10;
-	var barWidth = histWidth / bins;
+	var barWidth = histWidth / currentBins;
 	d3.select('#histogram').selectAll('.bar')
-	    .data(Array(bins))
+	    .data(Array(currentBins))
 	    .enter()
 	    .append('rect')
 	    .attr('class', 'bar')
@@ -389,34 +375,11 @@ function explore(data) {
 	    .attr('fill', 'steelblue')
 	    .attr('stroke', d3.rgb('steelblue').darker());
 
-	// default to selecting the first column of the csv for the histogram and y axis of scatterplot
+	// default to selecting the first column of the csv for the histogram
 	// later the user will be able to try all the different variables
-	d3.select('#selector1').select('text').text(varNames[0]);
-	d3.select('#yAxisLabel').text(varNames[0]);
-	var y = data.map(function(d) {return +d[varNames[0]]});
-	var yMin = d3.min(y);
-	var yMax = d3.max(y);
-	scatterScaleY = d3.scaleLinear().domain([Math.floor(yMin), Math.ceil(yMax)]).range([histHeight, 0]);
-
-	// default to selecting the second column of the csv for the x axis of scatterplot
-	d3.select('#selector2').select('text').text(varNames[1]);
-	var x = data.map(function(d) {return +d[varNames[1]]});
-	var xMin = d3.min(x);
-	var xMax = d3.max(x);
-	scatterScaleX = d3.scaleLinear().domain([Math.floor(xMin), Math.ceil(xMax)]).range([0, histWidth]);
-
-	// add points to the scatterplot
-	var blankArray = Array(data.length);
-	d3.select('#scatterplot').selectAll(".point")
-		.data(blankArray)
-		.enter().append("circle")
-		.attr("class", "point")
-		.attr("r", 3.5)
-		.attr("cx", function(d,i) {return(scatterScaleX(x[i]))})
-		.attr("cy", 0)
-		.style("fill", "none");
-
-	updateHist(y);
+	histLabel.text(varNames[0]);
+	currentVar = data.map(function(d) {return +d[varNames[0]]});
+	updateHist(currentVar, currentBins, 100);
 	
 
 } // end of explore
